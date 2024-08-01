37b6e63b0527ed64b427ec02748e17e4338af61a
Guilherme chaves <guigo4v40@gmail.com>
2024-08-01 13:55:35 -0300

    Teste

---
+++ Aquecimento3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+public class programa
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o nome da pessoa: ");
+        String Nome = Console.ReadLine();
+
+        Console.Write("Digite a idade da pessoa: ");
+        int Idade = int.Parse(Console.ReadLine());
+
+        if (Idade < 16)
+        {
+            Console.WriteLine("Não Pode votar");
+        }
+
+        else if (Idade >= 16 && Idade < 18)
+        {
+            Console.WriteLine("Voto Opcional");
+        }
+        else { Console.WriteLine("Pode votar"); }
+        }
+    }
+
